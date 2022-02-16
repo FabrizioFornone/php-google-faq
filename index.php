e4a391a3901e48eebf65c054ab9fe665de9e50f3
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-02-16 15:06:18 +0100

    header and nav done

--- index.php
+++ index.php
@@ -17,11 +17,43 @@ Gestire il “Database” e la visualizzazione di queste
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Php Google Faq Fabrizio Fornone</title>
 </head>
 
 <body>
+    <header class="py-3 px-4">
+        <div class="d-flex justify-content-between">
+
+            <div class="d-flex align-items-center">
+                <div>
+                    <img class="img-fluid" src="https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_74x24px.svg" alt="">
+                </div>
+                <div class="px-2 fs-5 fw-bold text-secondary privacy">Privacy e termini</div>
+            </div>
+            <div class="d-flex align-items-center">
+                <div class="px-3"> <a href="#">
+                        <i class="fas fa-th"></i>
+                    </a>
+                </div>
+                <div>
+                    <img id="profile" src="./img/SI_20190403_185523-modified.png" alt="">
+                </div>
+            </div>
+        </div>
+    </header>
+    <nav>
+        <ul class="d-flex">
+            <li> <a href="#">Introduzione</a> </li>
+            <li> <a href="#">Norme sulla privacy</a> </li>
+            <li> <a href="#">Termini di servizio </a> </li>
+            <li> <a href="#">Tecnologie</a> </li>
+            <li> <a href="#">Domande frequenti</a> </li>
+        </ul>
+        <hr>
+    </nav>
 
 </body>
 
