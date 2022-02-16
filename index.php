c298241431e7fbb2c4607bbb860f4bfe82dfc71f
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-02-16 14:19:09 +0100

    basic structure done

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+/* 
+Riscrivere questa pagina del sito google
+https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte.
+Gestire il “Database” e la visualizzazione di queste
+ domande e risposte con PHP.
+*/
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Php Google Faq Fabrizio Fornone</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
